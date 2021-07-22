6d97b3bee992395c47426bdca5fd55a8bb57c6d1
Mark Scherer <mark@Marks-MacBook-Pro.local>
2021-07-21 22:30:10 -0400

    Added google analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from "vue-gtag-next"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VueGtag, {
+  property: {
+    id: "G-DDB6VPT8Z5"
+  }
+})
+
+app.mount('#app')
