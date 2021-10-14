471749dd1c597966dd71838ae8b870c3d1d42aaa
Mark Scherer <mark@Marks-MacBook-Pro.local>
2021-10-13 21:55:04 -0700

    first round of questionnaire frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,54 +3,22 @@
 
     <div class="page">
 
-    <!-- navbar -->
-      <!-- inside page so logo and page contents aligned -->
-    <div class="navbar">
-      <div class="logo heading-font">SpinTheShaker</div>
-    </div>
-
-      <!-- input section -->
-      <div class="drinks-input">
-
-        <CollapsableInput v-model:selection="must_include_ingredients" v-model:expanded="expandedSelector.mustIncludeIngredients"
-          @update:expanded="handleCollaspables('mustIncludeIngredients', $event)"
-          :choices="ingredients" 
-          :preselects="preselects"
-          :expandedLabel="'Pick ingredients you want'" 
-          :sublabel="'Every drink will include ALL of these'"
-          :collapsedLabel="'OR pick ingredients you want'"
-          :placeholder="'Type to see more ingredients...'"
-          :inputId="'must-include-input'"
-        />
-
-        <CollapsableInput v-model:selection="preferred_ingredients" v-model:expanded="expandedSelector.preferredIngredients"
-          @update:expanded="handleCollaspables('preferredIngredients', $event)"
-          :choices="ingredients" 
-          :expandedLabel="'Pick ingredients you have'" 
-          :sublabel="'Every drink will use JUST these ingredients'"
-          :collapsedLabel="'OR pick ingredients you have'"
-          :placeholder="'Try \'whiskey\' or \'egg whites\''"
-          :inputId="'preferred-input'"
-        />
-        
-        <div class="drink-buttons">
-          <div class="get-drinks">
-            <button @click = 'updateAllDrinks'>{{updateDrinksButton}}</button>
-          </div>
-        </div>
+      <!-- navbar -->
+        <!-- inside page so logo and page contents aligned -->
+      <div class="navbar">
+        <div class="logo heading-font">SpinTheShaker</div>
       </div>
 
-      <!-- list of drinks  -->
-      <DrinkList 
-        :drinks="drinks" 
-        :totalDrinksCount="total_drinks_count" 
-        :loading="loading" 
-        :drinksLoaded="drinks_loaded" 
-        :showCountMsg="show_count_msg" 
-        :excluded_drinks="excluded_drinks"
-        @replaceDrink="replaceDrink"
+      <Questionnaire 
+        v-model:chosenDrinkNames="chosenDrinkNames"
+        v-model:unchosenDrinkNames="unchosenDrinkNames" 
+        :serverUrl="SERVER_URL"
+        @done="getDrinks"
       />
 
+      <DrinkList 
+        :drinks="drinks"
+      />
     </div>
   </div>
 </template>
@@ -58,81 +26,81 @@
 <script>
 const qs = require('qs')
 const _ = require('lodash')
-const utils = require('./incl/utils')
-const config = require('./incl/config')
+// const utils = require('./incl/utils')
+// const config = require('./incl/config')
 
+import Questionnaire from './components/Questionnaire.vue'
 import DrinkList from './components/DrinkList.vue'
-import CollapsableInput from './components/CollapsableInput.vue'
 
 // for dev, need to specify the port... dev server doesn't use express
 const SERVER_URL = process.env.NODE_ENV === 'development' ? `http://${window.location.hostname}:8000`: `http://${window.location.hostname}`
 
 // fetches ingredients from api
-const fetchIngredients = function() {
-  const url = new URL(`${SERVER_URL}/ingredients`)
-  return fetch(url)
-    .then(response => { 
-      if (response.status !== 200) {
-        console.error(`fetchIngredients got non-200 response: ${response.status}`)
-      }
-      return response.json()
-        .then(parsed_response => {
+// const fetchIngredients = function() {
+//   const url = new URL(`${SERVER_URL}/ingredients`)
+//   return fetch(url)
+//     .then(response => { 
+//       if (response.status !== 200) {
+//         console.error(`fetchIngredients got non-200 response: ${response.status}`)
+//       }
+//       return response.json()
+//         .then(parsed_response => {
           
-          // add desanitized ingredient fields
-          const individual_ingredients = _.map(parsed_response.ingredients, ingredient_info => {
-            return {
-              ...ingredient_info,
-              name: utils.desanitize(ingredient_info.ingredient),
-              category: utils.desanitize(ingredient_info.category)
-            }
-          })
+//           // add desanitized ingredient fields
+//           const individual_ingredients = _.map(parsed_response.ingredients, ingredient_info => {
+//             return {
+//               ...ingredient_info,
+//               name: utils.desanitize(ingredient_info.ingredient),
+//               category: utils.desanitize(ingredient_info.category)
+//             }
+//           })
 
-          // expand ingredient families & format as autocomplete choices
-          const families = _.map(parsed_response.families, (regex, parent) => {
-            return {
-              children: _.sortBy(_.filter(individual_ingredients, ingredient_info => RegExp(regex).test(ingredient_info.ingredient)), child => utils.desanitize(child.name)),
-              name: utils.desanitize(parent),
-              category: 'group'
-            }
-          })
+//           // expand ingredient families & format as autocomplete choices
+//           const families = _.map(parsed_response.families, (regex, parent) => {
+//             return {
+//               children: _.sortBy(_.filter(individual_ingredients, ingredient_info => RegExp(regex).test(ingredient_info.ingredient)), child => utils.desanitize(child.name)),
+//               name: utils.desanitize(parent),
+//               category: 'group'
+//             }
+//           })
 
-          return [ ...individual_ingredients, ...families ]
-        })
-    })
-    .catch(err => console.error(`error in fetchIngredients request: ${err}`))
-}
+//           return [ ...individual_ingredients, ...families ]
+//         })
+//     })
+//     .catch(err => console.error(`error in fetchIngredients request: ${err}`))
+// }
 
-const fetchDrinkRecs = function(n, _must_include_ingredients, _preferred_ingredients, only_preferred_ingredients, excluded_drinks, current_drinks) {
-  const url = new URL(`${SERVER_URL}/drinks/recs`)
-  const must_include_ingredients = _.map(_must_include_ingredients, ingredient => {
-    return ingredient.category === 'group' ?
-      _.map(ingredient.children, child => utils.sanitize(child.name)) :
-      [ utils.sanitize(ingredient.name) ]
-  })
-  const preferred_ingredients = _.map(_preferred_ingredients, ingredient => {
-    return ingredient.category === 'group' ?
-      _.map(ingredient.children, child => utils.sanitize(child.name)) :
-      [ utils.sanitize(ingredient.name) ]
-  })
-  const params = {
-    n,
-    must_include_ingredients,
-    preferred_ingredients,
-    only_preferred_ingredients,
-    alcoholic_drinks: true,
-    excluded_drinks,
-    current_drinks
-  }
-  url.search = qs.stringify(params, { encode: false })
+// const fetchDrinkRecs = function(n, _must_include_ingredients, _preferred_ingredients, only_preferred_ingredients, excluded_drinks, current_drinks) {
+//   const url = new URL(`${SERVER_URL}/drinks/recs`)
+//   const must_include_ingredients = _.map(_must_include_ingredients, ingredient => {
+//     return ingredient.category === 'group' ?
+//       _.map(ingredient.children, child => utils.sanitize(child.name)) :
+//       [ utils.sanitize(ingredient.name) ]
+//   })
+//   const preferred_ingredients = _.map(_preferred_ingredients, ingredient => {
+//     return ingredient.category === 'group' ?
+//       _.map(ingredient.children, child => utils.sanitize(child.name)) :
+//       [ utils.sanitize(ingredient.name) ]
+//   })
+//   const params = {
+//     n,
+//     must_include_ingredients,
+//     preferred_ingredients,
+//     only_preferred_ingredients,
+//     alcoholic_drinks: true,
+//     excluded_drinks,
+//     current_drinks
+//   }
+//   url.search = qs.stringify(params, { encode: false })
 
-  return fetch(url)
-    .then(response => {
-      if (response.status !== 200) {
-        console.error(`getDrinks got non-200 response: ${response.status}`)
-      }
-      return response.json()
-    })
-}
+//   return fetch(url)
+//     .then(response => {
+//       if (response.status !== 200) {
+//         console.error(`getDrinks got non-200 response: ${response.status}`)
+//       }
+//       return response.json()
+//     })
+// }
 
 const fetchDrinksInfo = function(drinks) {
   const url = new URL(`${SERVER_URL}/drinks/info`)
@@ -151,92 +119,49 @@ const fetchDrinksInfo = function(drinks) {
 export default {
   name: 'App',
   components: {
+    Questionnaire,
     DrinkList,
-    CollapsableInput,
-    // Autocomplete,
-    // Switch
   },
   data() {
     return {
-      /* user inputs state */
-      must_include_ingredients: [],
-      preferred_ingredients: [],
-      only_preferred_ingredients: false,
+      /* user inputs */
+      chosenDrinkNames: [],
+      unchosenDrinkNames: [],
 
       /* current results state */
-      ingredients: [],
       drinks: [],
-      total_drinks_count: 0,
       excluded_drinks: [],
 
       /* page lifecycle state */
-      expandedSelector: {
-        mustIncludeIngredients: true,
-        preferredIngredients: false
-      },
       loading: false,
       drinks_loaded: false,
-      show_count_msg: true
+
+      /* constants */
+      SERVER_URL: SERVER_URL
     }
   },
-  computed: {
-    preferredIngredientsPlaceholder() {
-      return this.only_preferred_ingredients ?
-        'Optional! All ingredients will come from this list' :
-        'Optional! We\'ll give preference to drinks with ingredients from this list'
-    },
-    updateDrinksButton() {
-      return this.drinks_loaded ?
-        'Get new drinks' :
-        'Get drinks'
-    },
-    preselects() {
-      return _.shuffle(this.ingredients).slice(0, _.random(config.MIN_PRESELECTS, config.MAX_PRESELECTS))
-    }
-  },
+  computed: {},
   methods: {
-    updateIngredients() {
-      fetchIngredients()
-        .then(ingredients => {
-          this.ingredients = ingredients
-          console.log(JSON.stringify({ ingredients: this.ingredients }))
-        })
-        .catch(err => console.error(`error running updateIngredients: ${err}`))
-    },
-    updateAllDrinks() {
-      this.excluded_drinks = []
-      this.drinks = []
-      this.loading = true
-      fetchDrinkRecs(null, this.must_include_ingredients, this.preferred_ingredients, this.only_preferred_ingredients, this.excluded_drinks)
-        .then(drinks_response => {
-          this.drinks = drinks_response.drinks
-          this.total_drinks_count = drinks_response.drink_count
+    // replaceDrink(index) {
+    //   console.log(`replacing drink: ${index}`)
+    //   const removed_drink = this.drinks.splice(index, 1)[0]
+    //   this.excluded_drinks.push(removed_drink.drink)
 
-          this.loading = false
-          this.drinks_loaded = true
-          this.show_count_msg = true
+    //   this.loading = true
+    //   fetchDrinkRecs(1, this.must_include_ingredients, this.preferred_ingredients, this.only_preferred_ingredients, this.excluded_drinks, _.map(this.drinks, 'drink'))
+    //     .then(drinks_response => {
+    //       this.total_drinks_count = this.drinks.length + drinks_response.drink_count
+    //       this.drinks = this.drinks.concat(drinks_response.drinks)
 
-          this.updateQueryString()
-        })
-    },
-    replaceDrink(index) {
-      console.log(`replacing drink: ${index}`)
-      const removed_drink = this.drinks.splice(index, 1)[0]
-      this.excluded_drinks.push(removed_drink.drink)
+    //       this.loading = false
+    //       this.drinks_loaded = true
+    //       this.show_count_msg = true
 
-      this.loading = true
-      fetchDrinkRecs(1, this.must_include_ingredients, this.preferred_ingredients, this.only_preferred_ingredients, this.excluded_drinks, _.map(this.drinks, 'drink'))
-        .then(drinks_response => {
-          this.total_drinks_count = this.drinks.length + drinks_response.drink_count
-          this.drinks = this.drinks.concat(drinks_response.drinks)
+    //       this.updateQueryString()
+    //     })
+    // },
 
-          this.loading = false
-          this.drinks_loaded = true
-          this.show_count_msg = true
-
-          this.updateQueryString()
-        })
-    },
+    // get info for drinks included in url querystring
     getUrlDrinksInfo() {
       const params = new URLSearchParams(window.location.search)
       const urlDrinks = params.get('drinks')
@@ -252,6 +177,8 @@ export default {
           })
       }
     },
+
+    // update url querystring with new drinks
     updateQueryString() {
       if (history.pushState) {
         const url = new URL(window.location)
@@ -261,14 +188,13 @@ export default {
         console.log(`updateQueryString: used pushHistory with ${url.toString()}`)
       } else console.error(`updateQueryString: cannot access history.pushState`)
     },
-    handleCollaspables(selector, nowExpanded) {
-      const otherSelector = selector === 'mustIncludeIngredients' ? 'preferredIngredients' : 'mustIncludeIngredients'
-      if (!nowExpanded) this.expandedSelector[otherSelector] = true // collapsing, open other selector
-      else this.expandedSelector[otherSelector] = false // expanding, close other selector
+
+    // get new drinks according to user inputs
+    getDrinks() {
+      console.log(`App: fetching drinks!`)
     }
   },
   mounted() {
-    this.updateIngredients(),
     this.getUrlDrinksInfo()
   }
 }
@@ -282,7 +208,7 @@ export default {
   /* app-wide formatting */
   .page {
     margin: 0 auto;
-    max-width: 95%;
+    max-width: 50%;
   }
 
   /* icon styling */
@@ -315,30 +241,19 @@ export default {
   }
 
   /* input section */
-  .drinks-input {
-    margin-bottom: 20px;
-  }
 
   /* input button styling */
-  .drink-buttons {
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-  }
-  .drink-buttons * {
-    margin: 0 10px;
-  }
-  button {
-    font: inherit;
-    font-size: 1.25em;
-    padding: 10px 40px;
-    border: none;
+  .button {
     cursor: pointer;
-    background: #d1cfcf;
-    outline: none;
+    background: lightgray;
+    margin: 5px 0;
+    padding: 5px;
   }
-  button:hover {
-    background: #a3a3a3;
+  .naked-button {
+    background: none;
+  }
+  .button:hover {
+    background: gray;
   }
 
   /* ingredient category styling */
---
+++ frontend/src/components/Questionnaire.vue
@@ -0,0 +1,218 @@
+<!-- Questionnaire: question machine to guide drink selection -->
+
+<template>
+  <div class="questionnaire">
+    <div class="heading heading-font">Let's find new drinks you'll love</div>
+
+    <div class="question-block" v-if="!loadingQuestion">
+      <div class="subheading">{{formatQuestion()}}</div>
+      <div 
+        v-for="(choice, index) in choices" :key="choice.drink" 
+        class="choice button"
+        v-on:click="pickedChoice(index)"
+      >
+        <div>{{formatChoice(choice)}}</div>
+      </div>
+      <div class="footer">
+        <div 
+          class="button naked-button"
+          v-on:click="pickedChoice()"
+        >None of these</div>
+        <div>Question {{formattedCount}}</div>
+      </div>
+    </div>
+    <div class="question-block-placeholder" v-if="loadingQuestion">
+      <div>loading...</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { desanitize } from '../incl/utils'
+const _ = require('lodash')
+// const utils = require('../incl/utils')
+
+const QUESTION_ENDPOINT = 'drinks/question'
+const TOTAL_QUESTIONS = 3
+
+const RECEIPE_QUESTIONS = [
+  'Which of these pairings sound best?',
+  'Which of these would you prefer?',
+  'Pick your favorite',
+  'What\'s your preference?',
+  'Any of these look interesting?'
+
+]
+const INGREDIENT_PRECEDENCE = [
+  'base_spirit',
+  'other_spirit',
+  'wine_/_beer',
+  'mixer',
+  'fruit_/_juice',
+  'liqueur_/_cordial',
+  'other_/_unknown',
+  'flavoring_/_syrup',
+  'herb_/_spice',
+  'garnish'
+]
+
+export default {
+  name: 'Questionnaire',
+  props: {
+    chosenDrinkNames: {
+      type: Array,
+      required: true,
+      description: 'two-way bound list of drink names selected by the user in question rounds'
+    },
+    unchosenDrinkNames: {
+      type: Array,
+      required: true,
+      description: 'two-way bound list of drink names not selected by the user in question rounds'
+    },
+    serverUrl:{
+      type: String,
+      required: true,
+      description: 'base url of server'
+    }
+  },
+  emits: ['update:chosenDrinkNames', 'update:unchosenDrinkNames', 'done'],
+  data() {
+    return {
+      /* class vars */
+      choices: [],
+      questionCount: 0,
+
+      /* UI state vars */
+      loadingQuestion: false,
+
+      /* constants */
+      TOTAL_QUESTIONS: TOTAL_QUESTIONS
+    }
+  },
+  computed: {
+    formattedCount: function() {
+      return `${this.questionCount} / ${TOTAL_QUESTIONS}`
+    }
+  },
+  methods: {
+    pickedChoice(index) {
+      console.log(`pickedChoice: ${index}`)
+      const _choices = _.cloneDeep(this.choices)
+
+      // updating two-way bound vars takes time, don't want to wait so we pass updated values directly into this.getNextQuestion()
+      let _chosenDrinkNames, _unchosenDrinkNames
+
+      // if no index, picked 'none of the above'
+      if (index !== null && index !== undefined) {
+        _chosenDrinkNames = this.chosenDrinkNames.concat([ this.choices[index].drink ])
+        this.$emit('update:chosenDrinkNames', _chosenDrinkNames)
+        _choices.splice(index, 1)
+      }
+      
+      _unchosenDrinkNames = this.unchosenDrinkNames.concat(_.map(_choices, 'drink'))
+      this.$emit('update:unchosenDrinkNames', _unchosenDrinkNames)
+      
+      if (this.questionCount < TOTAL_QUESTIONS) this.getNextQuestion(_chosenDrinkNames, _unchosenDrinkNames)
+      else this.$emit('done')
+    },
+
+    // get next question from server
+      // _chosenDrinkNames, _unchosenDrinkNames allows caller to override use of this.chosenDrinkNames, this.unchosenDrinkNames to prevent having to wait for them to update asynchornously
+    async getNextQuestion(_chosenDrinkNames, _unchosenDrinkNames) {
+      this.loadingQuestion = true
+
+      const url = new URL(QUESTION_ENDPOINT, this.serverUrl)
+      url.searchParams.set('chosenDrinkNames', _chosenDrinkNames || this.chosenDrinkNames)
+      url.searchParams.set('unchosenDrinkNames', _unchosenDrinkNames || this.unchosenDrinkNames)
+
+      let fullQuestionResponse, questionResponse
+      try {
+        questionResponse = await fetch(url, {method: 'GET'})
+          .then(rawResponse => {
+            fullQuestionResponse = rawResponse
+            return rawResponse.json()
+          })
+      } catch (getQuestionError) {
+        console.error(`Error: getNextQuestion: error getting question from server: ${JSON.stringify({ 
+          chosenDrinkNames: this.chosenDrinkNames, 
+          unchosenDrinkNames: this.unchosenDrinkNames,
+          getQuestionError: String(getQuestionError)
+        })}`)
+        return
+      }
+
+      if (fullQuestionResponse.status !== 200) {
+        console.error(`Error: getNextQuestion: error in server response: ${JSON.stringify({ 
+          chosenDrinkNames: this.chosenDrinkNames, 
+          unchosenDrinkNames: this.unchosenDrinkNames,
+          questionResponse
+        })}`)
+        return
+      }
+
+      console.log(`getNextQuestion: got new questions: ${JSON.stringify({ 
+        chosenDrinkNames: this.chosenDrinkNames, 
+        unchosenDrinkNames: this.unchosenDrinkNames,
+        questionResponse
+      })}`)
+
+      this.choices = questionResponse.choices
+      this.questionCount += 1
+      this.loadingQuestion = false
+    },
+
+    formatQuestion() {
+      return _.shuffle(RECEIPE_QUESTIONS)[0]
+    },
+    formatChoice(choice) {
+      return _.chain(choice.reciepe)
+        .sortBy(ingredientInfo => {
+          const category = ingredientInfo.ingredient_info.category
+          const precedence = INGREDIENT_PRECEDENCE.indexOf(category)
+          if (precedence < 0) throw Error(`Questionnire.formatChoice: did not know how to sort ingredient: ${JSON.stringify({ category })}`)
+          return precedence
+        })
+        .map(ingredientInfo => desanitize(ingredientInfo.ingredient))
+        .value()
+        .join(', ')
+    }
+  },
+  mounted() {
+    this.getNextQuestion()
+  }
+}
+</script>
+
+<style scoped>
+.questionnaire {
+  border: 1px solid black;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.heading {
+  font-size: larger;
+  font-weight: 600;
+  margin-bottom: 15px;
+  text-align: left;
+}
+.subheading {
+  font-size: medium;
+  margin-bottom: 10px;
+}
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 15px;
+}
+
+.question-block-placeholder {
+  height: 195px;
+  background: #cfcfcf;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
